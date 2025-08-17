68f052d123c65fe7724d600548ec01af055f49aa
Viktor Petruk <damnnn261@gmail.com>
2025-08-17 08:38:33 +0300

    native transition component, css styles for it

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const showMessage = ref(false)
+</script>
 
 <template>
-  <div>some</div>
+  <button @click="showMessage = !showMessage">
+    {{ showMessage ? 'Hide' : 'Show' }}
+    Menu
+  </button>
+
+  <Transition>
+    <ul v-if="showMessage">
+      <li>Menu Item 1</li>
+      <li>Menu Item 2</li>
+      <li>Menu Item 3</li>
+    </ul>
+  </Transition>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active, .v-leave-active {
+  transition: opacity 0.5s;
+}
+.v-enter, .v-leave-to {
+  opacity: 0;
+}
+</style>
