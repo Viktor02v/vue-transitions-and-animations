2e36a34d3a7ac17b1e4f6ff6ce85c4db83dd27dc
Viktor Petruk <damnnn261@gmail.com>
2025-08-17 09:20:57 +0300

    transition component props, named transitions

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+const show = ref(true)
 const showMessage = ref(false)
 </script>
 
 <template>
+  <Transition name="appear" :duration="1500" appear>
+    <div v-if="show">Hello there!!</div>
+  </Transition>
   <button @click="showMessage = !showMessage">
     {{ showMessage ? 'Hide' : 'Show' }}
     Menu
   </button>
 
-  <Transition>
+  <!-- Available props:
+        mode: 'out-in'
+        mode: 'in-out'
+        :duration=''
+        :key=''
+  -->
+  <Transition name="fade">
     <ul v-if="showMessage">
       <li>Menu Item 1</li>
       <li>Menu Item 2</li>
@@ -19,10 +29,28 @@ const showMessage = ref(false)
 </template>
 
 <style scoped>
-.v-enter-active, .v-leave-active {
-  transition: opacity 0.5s;
+/* Named Transitions to diversify */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease;
 }
-.v-enter, .v-leave-to {
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.appear-enter-active,
+.appear-leave-active {
+  transition: opacity 1500ms ease;
+}
+.appear-enter-from,
+.appear-leave-to {
   opacity: 0;
 }
 </style>
