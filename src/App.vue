19e0254a9d4a17b98df584129139bc7c279102f5
Viktor Petruk <damnnn261@gmail.com>
2025-08-17 10:00:49 +0300

    useing tailwind css with class override props

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,39 @@ const showMessage = ref(false)
 </script>
 
 <template>
-  <!-- Named Transition -->
-  <Transition name="appear" :duration="1500" appear>
-    <div v-if="show">Hello there!!</div>
-  </Transition>
-  <button @click="showMessage = !showMessage">
-    {{ showMessage ? 'Hide' : 'Show' }}
-    Menu
-  </button>
+  <div
+    class="w-full h-[100vh] flex flex-col space-y-[2rem] items-center justify-center"
+  >
+    <!-- Named Transition -->
+    <Transition name="appear" :duration="1500" appear>
+      <h1 class="text-2xl font-bold" v-if="show">Hello there!!</h1>
+    </Transition>
+    <div class="">
+      <button
+        class="bg-gray-100 border border-gray-300 p-2 rounded-md relative w-fill"
+        @click="showMessage = !showMessage"
+      >
+        {{ showMessage ? 'Hide' : 'Show' }}
+        Menu
+      </button>
 
-  <!-- Available props:
+      <!-- Available props:
         mode: 'out-in'
         mode: 'in-out'
         :duration=''
         :key=''
   -->
 
-  <!-- Reusable Transition -->
-  <FadeTransition>
-    <ul v-if="showMessage">
-      <li>Menu Item 1</li>
-      <li>Menu Item 2</li>
-      <li>Menu Item 3</li>
-    </ul>
-  </FadeTransition>
+      <!-- Reusable Transition -->
+      <FadeTransition>
+        <ul class="absolute w-full" v-if="showMessage">
+          <li>Menu Item 1</li>
+          <li>Menu Item 2</li>
+          <li>Menu Item 3</li>
+        </ul>
+      </FadeTransition>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- src/components/FadeTransition.vue
+++ src/components/FadeTransition.vue
@@ -1,12 +1,18 @@
 <template>
-  <Transition name="fade">
+  <!-- Class Override props 
+  enter-from-class
+  enter-active-class
+  enter-to-class
+  leave-from-class
+  leave-active-class
+  leave-to-class -->
+  <Transition enter-active-class="transition-opacity duration-500" name="fade">
     <slot />
   </Transition>
 </template>
 
 <style scoped>
 /* Named Transitions to diversify */
-.fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease, transform 0.5s ease;
 }
